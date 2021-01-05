89b381d9c55323651e7e414794a2374d55501a4c
pengyf <pengyf.3@jifenn.com>
2021-01-05 14:47:04 +0800

    update: recode app.js

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
+const fs = require('fs');
+const path = require('path')
 const Koa = require('koa');
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
-});
+const files = fs.readdirSync(path.resolve(__dirname, './controllers'));
+const js_files = files.filter((f) => {
+    return f.endsWith('.js');
+})
 
-router.post('/signin', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
+for (let f of js_files) {
+    console.log(`process controller: ${f}`);
+    let mapping = require(__dirname + '/controllers/' + f);
+    for (let url in mapping) {
+        if (url.startsWith('GET')) {
+            const path = url.substring(4);
+            router.get(path, mapping[url]);
+            console.log(`register URL mapping: GET ${path}`);
+        } else if (url.startsWith('POST')) {
+            const path = url.substring(5);
+            router.post(path, mapping[url]);
+            console.log(`register URL mapping: POST ${path}`);
+        } else {
+            console.log(`invalid url:${url}`);
+        }
     }
-});
+}
 
-app.use(bodyParser());
+app.use(bodyParser()); // bodyParser() 必须加在router被注册之前
 app.use(router.routes());
 
 app.listen(3000, () => {
